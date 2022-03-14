193e04058f0b428691bc011656a655485a1810b4
Steven Houglum <72063940+stevenh-ACM@users.noreply.github.com>
2022-03-13 18:02:53 -0700

    working!

--- Areas/Demo/Pages/SalesOrders/Details.cshtml.cs
+++ Areas/Demo/Pages/SalesOrders/Details.cshtml.cs
@@ -2,7 +2,9 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+
+using Acumatica.Auth.Api;
+using Acumatica.Default_20_200_001.Api;
 
 using AcuERP_DemoApp.Data;
 using AcuERP_DemoApp.Entities;
@@ -11,28 +13,144 @@ namespace AcuERP_DemoApp.Areas.Demo.Pages.SalesOrders;
 
 public class DetailsModel : PageModel
 {
+    private readonly ILogger<DetailsModel> _logger;
     private readonly AppDbContext _context;
 
-    public DetailsModel(AppDbContext context)
+    public DetailsModel(AppDbContext context, ILogger<DetailsModel> logger)
     {
         _context = context;
+        _logger = logger;
     }
 
-    public SalesOrder SalesOrder { get; set; }
+    /// <summary>
+    /// </summary>
+    [BindProperty]
+    public AcuCredential acuCredential { get; set; }
 
-    public async Task<IActionResult> OnGetAsync(int? id)
+    /// <summary>
+    /// </summary>
+    public string ReturnUrl { get; set; }
+
+    /// <summary>
+    /// </summary>
+    [TempData]
+    public string ErrorMessage { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int id)
     {
-        if (id == null)
-        {
-            return NotFound();
-        }
+        acuCredential = await _context.AcuCredentials.FindAsync(id);
+        Console.WriteLine("acuCredential to use: " + acuCredential);
 
-        SalesOrder = await _context.SalesOrders.FirstOrDefaultAsync(m => m.Id == id);
-
-        if (SalesOrder == null)
+        if (acuCredential == null)
         {
             return NotFound();
         }
         return Page();
     }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        //if (!ModelState.IsValid)
+        //{
+        //    return RedirectToPage("/Index");
+        //}
+
+        acuCredential = await _context.AcuCredentials.FindAsync(1);
+
+        //acuCredential acuCredential = _context.acuCredentials.FirstOrDefault();
+        Console.WriteLine("REST API example using acuCredential:" + acuCredential);
+
+        //var authApi = new AuthApi(acuCredential.SiteUrl,
+        //     requestInterceptor: Logger.LogRequest, responseInterceptor: Logger.LogResponse);
+
+        var authApi = new AuthApi(acuCredential.siteUrl,
+            requestInterceptor: Logger.LogRequest, responseInterceptor: Logger.LogResponse);
+
+        try
+        {
+            //var configuration = authApi.LogIn(acuCredential.UserId, acuCredential.Password, acuCredential.Tenant, acuCredential.Branch, acuCredential.Locale);
+
+            var configuration = authApi.LogIn(acuCredential.userName, acuCredential.password, "", "", "");
+
+            Console.WriteLine("Reading Accounts...");
+            var accountApi = new AccountApi(configuration);
+            var salesOrderApi = new SalesOrderApi(configuration);
+            var accounts = accountApi.GetList(top: 99);
+            var salesOrders = salesOrderApi.GetList(top: 22);
+            foreach (var account in accounts)
+            {
+                Console.WriteLine("Account Nbr: " + account.AccountCD.Value + "/" + account.Description.Value + ";");
+            }
+            foreach (var order in salesOrders)
+            {
+                Console.WriteLine("Sales Order Nbr: " + order.OrderNbr.Value + "/" + order.OrderTotal.Value + ";");
+            }
+
+            //Console.WriteLine("Reading Sales Order by Keys...");
+            //var salesOrderApi = new SalesOrderApi(configuration);
+            //var order = salesOrderApi.GetByKeys(new List<string>() { "SO", "SO006596" });
+            //Console.WriteLine("Order Total: " + order.OrderTotal.Value);
+
+
+            //var shipmentApi = new ShipmentApi(configuration);
+            //var shipment = shipmentApi.GetByKeys(new List<string>() { "004202" });
+            //Console.WriteLine("ConfirmShipment");
+            //shipmentApi.WaitActionCompletion(shipmentApi.InvokeAction(new ConfirmShipment(shipment)));
+
+            //Console.WriteLine("CorrectShipment");
+            //shipmentApi.WaitActionCompletion(shipmentApi.InvokeAction(new CorrectShipment(shipment)));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            //we use logout in finally block because we need to always logout, even if the request failed for some reason
+
+            authApi.Logout();
+            Console.WriteLine("Logged Out...");
+
+        }
+        return RedirectToPage("/Index");
+    }
 }
+
+//#nullable disable
+
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Microsoft.EntityFrameworkCore;
+
+//using AcuERP_DemoApp.Data;
+//using AcuERP_DemoApp.Entities;
+
+//namespace AcuERP_DemoApp.Areas.Demo.Pages.SalesOrders;
+
+//public class DetailsModel : PageModel
+//{
+//    private readonly AppDbContext _context;
+
+//    public DetailsModel(AppDbContext context)
+//    {
+//        _context = context;
+//    }
+
+//    public SalesOrder SalesOrder { get; set; }
+
+//    public async Task<IActionResult> OnGetAsync(int? id)
+//    {
+//        if (id == null)
+//        {
+//            return NotFound();
+//        }
+
+//        SalesOrder = await _context.SalesOrders.FirstOrDefaultAsync(m => m.Id == id);
+
+//        if (SalesOrder == null)
+//        {
+//            return NotFound();
+//        }
+//        return Page();
+//    }
+//}
--- Auxillary/Logger.cs
+++ Auxillary/Logger.cs
@@ -1,6 +1,49 @@
-﻿namespace AcuERP_DemoApp.Auxillary
+﻿
+using RestSharp;
+
+public static class Logger
 {
-    public class Logger
+    private const string _requestsLogPath = "RequestsLog.txt";
+    public static void LogResponse(RestRequest request, RestResponse response, RestClient restClient)
     {
+        StreamWriter writer = new StreamWriter(_requestsLogPath, true);
+        writer.WriteLine(DateTime.Now.ToString());
+        writer.WriteLine("Response");
+        writer.WriteLine("\tStatus code: " + response.StatusCode);
+        writer.WriteLine("\tContent: " + response.Content);
+        writer.WriteLine("-----------------------------------------");
+        writer.WriteLine();
+        writer.Flush();
+        writer.Close();
+
+    }
+    public static void LogRequest(RestRequest request, RestClient restClient)
+    {
+        StreamWriter writer = new StreamWriter(_requestsLogPath, true);
+        writer.WriteLine(DateTime.Now.ToString());
+        writer.WriteLine("Request");
+        writer.WriteLine("\tMethod: " + request.Method);
+        string parameters = "";
+        string body = "";
+        foreach (var parametr in request.Parameters)
+        {
+            if (parametr.Type == ParameterType.QueryString)
+            {
+                parameters += String.IsNullOrEmpty(parameters) ? "?" : "&";
+                parameters += parametr.Name + "=" + parametr.Value;
+            }
+
+            if (parametr.Type == ParameterType.RequestBody)
+                body += parametr.Value;
+        }
+
+        writer.WriteLine("\tURL: " + restClient.BuildUri(request) + parameters);
+        if (!String.IsNullOrEmpty(body))
+            writer.WriteLine("\tBody: " + body);
+        writer.WriteLine("-----------------------------------------");
+        writer.WriteLine();
+        writer.Flush();
+
+        writer.Close();
     }
 }
